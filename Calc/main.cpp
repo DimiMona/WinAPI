bb5976b34bb761a23e61cb7bb1ebf53b9085298c
Mona Dmitriy <skorobei91@mail.ru>
2026-01-22 21:27:40 +0300

    Font DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -13,7 +13,9 @@
 #define g_i_START_X				10
 #define g_i_START_Y				10
 #define g_i_DISPLAY_WIDTH		g_i_BUTTON_SIZE * 5 + g_i_INTERVAL * 4
-#define g_i_DISPLAY_HEIGHT		22
+#define g_i_DISPLAY_HEIGHT		g_i_BUTTON_SIZE
+#define g_i_FONT_HEIGHT			(g_i_DISPLAY_HEIGHT-2)
+#define g_i_FONT_WIDTH			g_i_FONT_HEIGHT/2.5
 
 #define g_i_BUTTON_START_X		g_i_START_X
 #define g_i_BUTTON_START_Y		g_i_START_Y + g_i_DISPLAY_HEIGHT + g_i_INTERVAL
@@ -94,6 +96,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	static Skin skin = Skin::SquareBlue;
+	static HFONT hFont = NULL;
 		switch (uMsg)
 		{
 		case WM_CREATE:
@@ -114,6 +117,23 @@ LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				GetModuleHandle(NULL),
 				NULL
 			);
+			AddFontResourceEx ("Fount\\digital-7 (mono).ttf", FR_PRIVATE, 0);
+			hFont = CreateFont
+			(
+				g_i_FONT_HEIGHT, g_i_FONT_WIDTH,
+				0, 0, 500, // Escapement,Orientation,Width
+				FALSE, FALSE, FALSE, // Italic,Underline,strikeout
+				DEFAULT_CHARSET,
+				OUT_DEFAULT_PRECIS,
+				CLIP_DEFAULT_PRECIS,
+				ANTIALIASED_QUALITY,
+				DEFAULT_PITCH,
+				"Digital-7 Mono"
+				//"DS-Digital"
+			);
+			SendMessage(hEdit, WM_SETFONT, (WPARAM)hFont, TRUE);
+			/*DeleteObject(hFont);
+			hFont = NULL;*/
 			////////////////////////////////////////////////////////////
 			CHAR sz_digit[2] = {};
 			for (int i = 6; i >= 0; i -= 3)
@@ -468,6 +488,7 @@ LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		break;
 		case WM_DESTROY:
 			FreeConsole();
+			DeleteObject(hFont);
 			PostQuitMessage(0);
 			break;
 
